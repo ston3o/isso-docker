22ab0875c2800c2ac9125877fa1f69166ffeacbf
ston3o <contact@ston3o.me>
2019-01-14 10:40:18 +0100

    Add SMTP params

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ dbpath = /data/isso.db
 name = ${ISSO_NAME:-comments.example.com}
 host = ${ISSO_HOST:-http://example.com}
 max-age = ${ISSO_MAX_AGE:-30m}
+admin_password = ${ISSO_ADMIN_PASSWORD:-secret}
+notify = ${ISSO_NOTIFY:-stdout}
 
 [server]
 listen = http://0.0.0.0:80
@@ -17,6 +19,16 @@ direct-reply = 3
 reply-to-self = ${ISSO_REPLY_TO_SELF:-false}
 require-email = ${ISSO_REQUIRE_EMAIL:-false}
 require-author = ${ISSO_REQUIRE_AUTHOR:-false}
+
+[smtp]
+username = ${ISSO_SMTP_USERNAME}
+password = ${ISSO_SMTP_PASSWORD}
+host = ${ISSO_SMTP_HOST:-localhost}
+port = ${ISSO_SMTP_PORT:-587}
+security = ${ISSO_SMTP_SECURITY:-starttls}
+to = ${ISSO_SMTP_TO}
+from = ${ISSO_SMTP_FROM}
+timeout = 10
 
 EOF
 
